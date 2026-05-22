f6771645e1180f21b333636444c2af99cfc98981
개발자1 <cm41823@gmail.com>
2026-05-22 17:29:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -0,0 +1,20 @@
+package com.back.simpleDb;
+
+
+import org.springframework.test.context.jdbc.Sql;
+
+public class SimpleDb {
+    public void setDevMode(boolean b) {
+    }
+
+    public void run(String dropTableIfExistsArticle) {
+    }
+
+    public SimpleDb(String localhost, String root, String root123414, String simpleDb__test) {
+    }
+
+    public void run(String s, String title, String body, boolean isBlind) {
+    }
+
+
+}
