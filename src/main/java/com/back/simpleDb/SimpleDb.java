75aa3f42c87694dd8183ff6aaa8063cbc8159484
개발자1 <cm41823@gmail.com>
2026-05-22 17:48:52 +0900

    test: t001() 레드 케이스

--- src/main/java/com/back/simpleDb/SimpleDb.java
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -1,9 +1,13 @@
 package com.back.simpleDb;
 
 
-import org.springframework.test.context.jdbc.Sql;
-
 public class SimpleDb {
+    //testcode에 SimpleDb 파라미터를 저장할 변수 선언
+    private String host;
+    private String userName;
+    private String password;
+    private String dbName;
+
     public void setDevMode(boolean b) {
     }
 
@@ -11,10 +15,15 @@ public class SimpleDb {
     }
 
     public SimpleDb(String localhost, String root, String root123414, String simpleDb__test) {
+        this.host = host;
+        this.userName = userName;
+        this.password = password;
+        this.dbName = dbName;
     }
 
     public void run(String s, String title, String body, boolean isBlind) {
     }
-
-
+    public Sql genSql() {
+        return new Sql();
+    }
 }
--- src/main/java/com/back/simpleDb/Sql.java
+++ src/main/java/com/back/simpleDb/Sql.java
@@ -1,4 +1,11 @@
 package com.back.simpleDb;
 
 public class Sql {
+    public Sql append(String sqlBit, Object... params) {
+        return this;
+    }
+
+    public long insert() {
+    return 0;
+    }
 }
